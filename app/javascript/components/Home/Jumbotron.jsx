44eb2a6a54c4522d0d4318f726bd3c1bc49550e7
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-08 22:48:50 +0800

    Style components for jumbotron

--- app/javascript/components/Home/Jumbotron.jsx
+++ app/javascript/components/Home/Jumbotron.jsx
@@ -1,14 +1,53 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+  background-color: #d74234;
+  min-height: 550px;
+  padding: 100px 0;
+  color: #fff;
+`
+
+const Header = styled.h1`
+  color: #fff;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 52px;
+`
+
+const Subhead = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+`
+
+const Button = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 0;
+  background: #fff;
+  color: #333 !important;
+  padding: 10px 20px;
+  font-size: 18px;
+  // width: 100%;
+  box-shadow: 0px 0px 0px 3px #473228,
+              -6px 6px #ef5f17,
+              -6px 6px 0px 3px #473228;
+`
 
 const Jumbotron = () => {
   return (
-    <section className="home-section--1">
+    <Section className="home-section--1">
       <div className="container">
         <div className="row">
           <div className="col col-sm-12 col-md-5">
             <div className="pt-4 mt-4">
-              <h1>React For Rails Developers</h1>
-              <p>Supercharge your Ruby on Rails Apps with React.js</p>
+              <Header>React For Rails Developers</Header>
+              <Subhead>Supercharge your Ruby on Rails Apps with React.js</Subhead>
+              <div className="cta-wrapper">
+                <Button className="btn fancy-btn">Get Started</Button>
+              </div>
             </div>
           </div>
           <div className="col col-sm-12 col-md-7">
@@ -18,7 +57,7 @@ const Jumbotron = () => {
           </div>
         </div>
       </div>
-    </section>
+    </Section>
   )
 }
 

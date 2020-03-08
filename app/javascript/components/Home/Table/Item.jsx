506701f83fb0bcf5edc6078aac438a5d4b7d7401
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-08 23:07:04 +0800

    Add item component

---
+++ app/javascript/components/Home/Table/Item.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+const Item = (props) => {
+  return(
+    <div className="row">
+      <div className="col-md-10 offset-md-1">
+        <div className="tect-center">
+          <div className="card px-5">
+            <div className="row">
+              <div className="col-md-4"></div>
+              <div className="col-md-8"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Item

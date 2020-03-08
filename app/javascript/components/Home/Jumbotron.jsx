0ef2fd8ce46bffe16b7e3f0a6ced60f1c25c1c92
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-08 18:51:06 +0800

    Add jumbotron to home

---
+++ app/javascript/components/Home/Jumbotron.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+const Jumbotron = () => {
+  return (
+    <section className="home-section--1">
+      <div className="container">
+        <div className="row">
+          <div className="col col-sm-12 col-md-5">
+            <div className="pt-4 mt-4">
+              <h1>React For Rails Developers</h1>
+              <p>Supercharge your Ruby on Rails Apps with React.js</p>
+            </div>
+          </div>
+          <div className="col col-sm-12 col-md-7">
+            <div className="pt-4 mt-4 text-center">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/5F_JUvPq410" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Jumbotron

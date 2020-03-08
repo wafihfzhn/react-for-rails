2ae0872e2344f0492a4b5d3f97a9f438959238e4
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-09 00:21:04 +0800

    Style button of watch video of course modules

--- app/javascript/components/Home/Table/Item.jsx
+++ app/javascript/components/Home/Table/Item.jsx
@@ -1,5 +1,22 @@
 import React, { Component } from 'react'
 import Thumbnail from './Thumbnail'
+import styled from 'styled-components';
+
+const Button = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 0;
+  background: #fff;
+  color: #333 !important;
+  padding: 10px 20px;
+  font-size: 18px;
+  // width: 100%;
+  box-shadow: 0px 0px 0px 3px #473228,
+              -6px 6px #ef5f17,
+              -6px 6px 0px 3px #473228;
+`
 
 const Item = (props) => {
   return(
@@ -13,10 +30,10 @@ const Item = (props) => {
               </div>
               <div className="col-md-8">
                 <div className="pt-4 pb-4">
-                  <h4>{props.title}</h4>
+                  <h5>{props.title}</h5>
                   <p>{props.description}</p>
                   <div className="cta-wrapper">
-                    <a className="btn cta-btn">Watch This Video</a>
+                    <Button className="btn cta-btn">Watch This Video</Button>
                   </div>
                 </div>
               </div>

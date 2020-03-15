07dfbb201af605a07aed8b22b25619f7ecde2d4d
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-15 21:49:52 +0800

    Add dummy data for course episode and section

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating Course'
+course = Course.create(title: 'Hello World', description: 'Create a react app with Ruby on Rails')
+
+puts 'Creating Section'
+section = Section.create(title: 'Chapter 1', course: course)
+
+puts 'Creating Episodes'
+episodes = Episode.create([
+  { title: '1. Setting up a new Ruby on Rails App with React.', description: 'Lorem Ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+  { title: '2. Adding React to an Existing Rails App.', description: 'Lorem Ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+  { title: '3. Building a Hello Wordl App.', description: 'Lorem Ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section },
+  { title: '4. Adding React Router Dom to your App', description: 'Lorem Ipsum', url: 'https://www.youtube.com/embed/5F_JUvPq410', section: section }
+])

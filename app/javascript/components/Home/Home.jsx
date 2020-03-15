81bea900f9b405d19a58e5617e026804c503a180
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-15 22:29:56 +0800

    Get data from database on home react

--- app/javascript/components/Home/Home.jsx
+++ app/javascript/components/Home/Home.jsx
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import Jumbotron from './Jumbotron';
 import Table from './Table/Table';
+import axios from 'axios'
 
 class Home extends Component {
   constructor(){
     super()
 
     this.state = {
-      course_modules: [
-        { id: 1, title: '1. Setting up a new Ruby on Rails App with React.', description: 'Lorem Ipsum', active: false },
-        { id: 2, title: '2. Adding React to an Existing Rails App.', description: 'Lorem Ipsum', active: false },
-        { id: 3, title: '3. Building a Hello Wordl App.', description: 'Lorem Ipsum', active: false },
-        { id: 4, title: '4. Adding React Router Dom to your App', description: 'Lorem Ipsum', active: false }
-      ]
+      course_modules: []
     }
+  }
+
+  componentDidMount(){
+    axios.get('/episodes.json')
+    .then(data => {
+      debugger
+      let res = []
+      data.data.data.map( (data) => {
+        res.push({id: data.id, title: data.title, description: data.description, active: false })
+
+        this.setState({course_modules: res})
+      })
+    })
+    .catch(data => {
+      debugger
+    })
   }
 
   handleVideoChange(item, event){

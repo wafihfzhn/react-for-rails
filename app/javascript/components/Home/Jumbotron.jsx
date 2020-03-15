a35d5e9f1fd9f17c1dc4c7042980e6fc9fc0f905
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-15 21:49:16 +0800

    Modification of jumbotron on react

--- app/javascript/components/Home/Jumbotron.jsx
+++ app/javascript/components/Home/Jumbotron.jsx
@@ -41,7 +41,7 @@ const Jumbotron = () => {
     <Section className="home-section--1">
       <div className="container">
         <div className="row">
-          <div className="col col-sm-12 col-md-5">
+          <div className="col-sm-12 col-md-5">
             <div className="pt-4 mt-4">
               <Header>React For Rails Developers</Header>
               <Subhead>Supercharge your Ruby on Rails Apps with React.js</Subhead>
@@ -50,9 +50,11 @@ const Jumbotron = () => {
               </div>
             </div>
           </div>
-          <div className="col col-sm-12 col-md-7">
+          <div className="col-sm-12 col-md-7">
             <div className="pt-4 mt-4 text-center">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/5F_JUvPq410" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              <iframe width="100%" height="315" src="https://www.youtube.com/embed/5F_JUvPq410"
+                      frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen></iframe>
             </div>
           </div>
         </div>

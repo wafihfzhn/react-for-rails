8b72a19376e9b86776ba54200f242b16468df025
wafihfzhn <hafizhuddin96@gmail.com>
2020-03-16 09:17:45 +0800

    Update jumbotrun on hone

--- app/javascript/components/Home/Jumbotron.jsx
+++ app/javascript/components/Home/Jumbotron.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Section = styled.section`
   background-color: #d74234;
-  min-height: 550px;
+  min-height: 350px;
   padding: 100px 0;
   color: #fff;
 `
@@ -20,41 +20,15 @@ const Subhead = styled.p`
   font-weight: 500;
 `
 
-const Button = styled.a`
-  display: inline-block;
-  text-decoration: none;
-  font-weight: bold;
-  cursor: pointer;
-  border-radius: 0;
-  background: #fff;
-  color: #333 !important;
-  padding: 10px 20px;
-  font-size: 18px;
-  // width: 100%;
-  box-shadow: 0px 0px 0px 3px #473228,
-              -6px 6px #ef5f17,
-              -6px 6px 0px 3px #473228;
-`
-
 const Jumbotron = () => {
   return (
     <Section className="home-section--1">
-      <div className="container">
+      <div className="container text-center">
         <div className="row">
-          <div className="col-sm-12 col-md-5">
-            <div className="pt-4 mt-4">
+          <div className="col-sm-12 col-md-12">
+            <div className="pt-4">
               <Header>React For Rails Developers</Header>
               <Subhead>Supercharge your Ruby on Rails Apps with React.js</Subhead>
-              <div className="cta-wrapper">
-                <Button className="btn fancy-btn">Get Started</Button>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-12 col-md-7">
-            <div className="pt-4 mt-4 text-center">
-              <iframe width="100%" height="315" src="https://www.youtube.com/embed/5F_JUvPq410"
-                      frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen></iframe>
             </div>
           </div>
         </div>
